5e25352bfa7a3d3dd798c275dc783557decbbfe3
jsglen1 <wtfdxd@gmail.com>
2023-01-25 15:35:32 -0500

    remove task

--- app/src/main/java/com/example/notesapp/addTasks/ui/TaskScreen.kt
+++ app/src/main/java/com/example/notesapp/addTasks/ui/TaskScreen.kt
@@ -1,7 +1,10 @@
 package com.example.notesapp.addTasks.ui
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -14,10 +17,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
+import com.example.notesapp.addTasks.ui.model.TaskModel
 
 //@Preview(showBackground = true, showSystemUi = true)
 @Composable
@@ -33,14 +38,64 @@ fun TaskScreen(taskViewModel: TaskViewModel) {
         FabDialog(
             Modifier
                 .align(Alignment.BottomEnd)
-                .padding(16.dp)
-        ,taskViewModel
+                .padding(16.dp), taskViewModel
         )
+        TasksList(taskViewModel)
     }
 }
 
 @Composable
-fun FabDialog(modifier: Modifier,taskViewModel: TaskViewModel) {
+fun TasksList(taskViewModel: TaskViewModel) {
+
+    val myTask: List<TaskModel> = taskViewModel.tasks
+
+    LazyColumn {
+        items(myTask, key = { it.id }) {
+            ItemTask(it, taskViewModel)
+        }
+    }
+}
+
+//@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun ItemTask(taskModel: TaskModel, taskViewModel: TaskViewModel) {
+    Card(
+        Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 8.dp).pointerInput(Unit){
+                detectTapGestures (onLongPress = {
+                    taskViewModel.onItemRemove(taskModel)
+                })
+            },
+        elevation = 8.dp
+    ) {
+
+        Row(
+            Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = taskModel.task,
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(4.dp),
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold,
+            )
+            Checkbox(
+                checked = taskModel.selected,
+                onCheckedChange = { taskViewModel.onCheckBox(taskModel) })
+        }
+
+    }
+
+
+}
+
+@Composable
+fun FabDialog(modifier: Modifier, taskViewModel: TaskViewModel) {
     FloatingActionButton(
         onClick = { taskViewModel.onShowDialogClick() },
         modifier = modifier
@@ -76,7 +131,10 @@ fun AddTaskDialog(show: Boolean, onDismiss: () -> Unit, onTaskAdded: (String) ->
                     maxLines = 1
                 )
                 Spacer(modifier = Modifier.size(16.dp))
-                Button(onClick = { onTaskAdded(text) }, Modifier.fillMaxWidth()) {
+                Button(onClick = {
+                    onTaskAdded(text)
+                    text = ""
+                }, Modifier.fillMaxWidth()) {
                     Text(text = "Añadir Tarea")
                 }
             }
--- app/src/main/java/com/example/notesapp/addTasks/ui/TaskViewModel.kt
+++ app/src/main/java/com/example/notesapp/addTasks/ui/TaskViewModel.kt
@@ -1,9 +1,11 @@
 package com.example.notesapp.addTasks.ui
 
 import android.util.Log
+import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.notesapp.addTasks.ui.model.TaskModel
 import javax.inject.Inject
 
 class TaskViewModel @Inject constructor() : ViewModel() {
@@ -11,16 +13,30 @@ class TaskViewModel @Inject constructor() : ViewModel() {
     private val _showDialog = MutableLiveData<Boolean>()
     val showDialog: LiveData<Boolean> = _showDialog
 
+    private val _tasks = mutableStateListOf<TaskModel>()
+    val tasks: List<TaskModel> = _tasks
+
     fun onDialogClose() {
         _showDialog.value = false
     }
 
     fun onTaskCreated(text: String) {
+        _tasks.add(TaskModel(task = text))
         Log.i("glen", text)
         onDialogClose()
     }
 
     fun onShowDialogClick() {
         _showDialog.value = true
+    }
+
+    fun onCheckBox(taskModel: TaskModel) {
+        val index = _tasks.indexOf(taskModel)
+        _tasks[index] = _tasks[index].let { it.copy(selected = !it.selected) }
+    }
+
+    fun onItemRemove(taskModel: TaskModel) {
+        val task = _tasks.find { it.id == taskModel.id }
+        _tasks.remove(task)
     }
 }
