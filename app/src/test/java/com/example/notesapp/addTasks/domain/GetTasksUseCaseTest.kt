ac16d1e5f8aab25e7a925ca20cdf98a63caeb017
jsglen1 <wtfdxd@gmail.com>
2023-01-27 19:39:05 -0500

    testing

---
+++ app/src/test/java/com/example/notesapp/addTasks/domain/GetTasksUseCaseTest.kt
@@ -0,0 +1,44 @@
+package com.example.notesapp.addTasks.domain
+
+import com.example.notesapp.addTasks.data.TaskRepository
+import com.example.notesapp.addTasks.ui.model.TaskModel
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.impl.annotations.RelaxedMockK
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+
+
+internal class GetTasksUseCaseTest{
+
+    @RelaxedMockK
+    private lateinit var taskRepository: TaskRepository
+
+    lateinit var getTasksUseCase: GetTasksUseCase
+
+    @Before
+    fun onBefore() {
+        MockKAnnotations.init(this)
+        getTasksUseCase = GetTasksUseCase(taskRepository)
+    }
+
+    @Test
+    fun `when the getTaskUseCase call data TaskRepository them  not null`() = runBlocking {
+
+        val task = TaskModel(18,"funiona?",false)
+
+        //given
+        coEvery { taskRepository.addTask(task) }
+
+        //when
+        val rep = getTasksUseCase()
+        //Log.i("test", rep.toString())
+
+        //them
+        //coVerify { rep.collect{ it == rep } }
+        assert(rep != null)
+
+    }
+
+}
